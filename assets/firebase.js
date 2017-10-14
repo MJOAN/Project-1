a77ae79628a585bae8210542487a92779ee4aea3
Mariam Joan <MariamJoan@Mariams-MacBook-Pro.local>
2017-10-13 20:53:18 -0700

    10-13: reworking fb code

--- assets/firebase.js
+++ assets/firebase.js
@@ -13,22 +13,43 @@ var password = "";
 var todaysDate = "10-13-17";
 
 
-$("#click-button").on("click", function(event) {
-            event.preventDefault();
+$(document).on("click", "register", function(event) {
+    event.preventDefault();
 
-            displayName = $("#name").val().trim();
-            age = $("#age").val().trim();
-            email = $("#email").val().trim();
-            password = $("#password").val().trim();
+    displayName = $("#name").val().trim();
+    age = $("#age").val().trim();
+    email = $("#email").val().trim();
+    password = $("#password").val().trim();
 
-            usersRef.child(currentUID).set({
-                name: displayName,
-                age: age
-            }).then(function(data) {
-                console.log(data);
-                window.location.assign("index.html");
-            })
+    usersRef.child(currentUID).set({
+        name: displayName,
+        age: age
+    }).then(function(data) {
+        console.log(data);
+        window.location.assign("index.html");
+    })
 
-            });
+});
 
 
+$(document).on("click", "#add-user-data-btn", function(event) {
+    var activities = $("activities").val();
+    var symptoms = $("symptoms").val();
+    var alcohol = $("alcohol").val();
+    var location = $("location").val().trim();
+    var comments = $("comments").val().trim();
+
+    console.log(location, "hit");
+
+
+    user.ref(`users/` + user.uid + `/form`).set({
+        activities: activities,
+        symptoms: symptoms,
+        location: location,
+        alcohol: alcohol,
+        comment: comments
+
+    }).then(function(data) {
+        console.log(activities);
+        console.log("hit");
+    })
